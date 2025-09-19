1ce81d7bd1c18c11de6a00d3ad489a282090004c
Michael Busbee <mbusbee505@gmail.com>
2025-09-19 16:46:08 -0400

    vault backup: 2025-09-19 16:46:07

--- source/quartz.config.ts
+++ source/quartz.config.ts
@@ -16,7 +16,7 @@ const config: QuartzConfig = {
       provider: "plausible",
     },
     locale: "en-US",
-    baseUrl: "",
+    baseUrl: "mbusbee505.github.io/TechBook",
     ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "created",
     theme: {
