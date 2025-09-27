1cbf47e8ae28732b6df70458ebd3c38982ddf215
Michael Busbee <mbusbee505@gmail.com>
2025-09-26 21:24:41 -0400

    vault backup: 2025-09-26 21:24:41

--- source/quartz.layout.ts
+++ source/quartz.layout.ts
@@ -29,7 +29,7 @@ export const defaultContentPageLayout: PageLayout = {
     Component.MobileOnly(Component.Spacer()),
     Component.Search(),
     Component.Darkmode(),
-    Component.DesktopOnly(Component.Explorer()),
+    Component.Explorer(),
   ],
   right: [
     Component.DesktopOnly(Component.TableOfContents()),
@@ -46,7 +46,7 @@ export const defaultListPageLayout: PageLayout = {
     Component.MobileOnly(Component.Spacer()),
     Component.Search(),
     Component.Darkmode(),
-    Component.DesktopOnly(Component.Explorer()),
+    Component.Explorer(),
   ],
   right: [],
 }
