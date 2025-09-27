e18656056d7dab65bbd6b7733254bc03b6293dc5
Michael Busbee <mbusbee505@gmail.com>
2025-09-26 22:01:43 -0400

    vault backup: 2025-09-26 22:01:43

--- source/quartz/components/Footer.tsx
+++ source/quartz/components/Footer.tsx
@@ -1,15 +1,12 @@
 import { QuartzComponent, QuartzComponentConstructor, QuartzComponentProps } from "./types"
 import style from "./styles/footer.scss"
-import { version } from "../../package.json"
-import { i18n } from "../i18n"
 
 interface Options {
   links: Record<string, string>
 }
 
 export default ((opts?: Options) => {
-  const Footer: QuartzComponent = ({ displayClass, cfg }: QuartzComponentProps) => {
-    const year = new Date().getFullYear()
+  const Footer: QuartzComponent = ({ displayClass }: QuartzComponentProps) => {
     const links = opts?.links ?? []
     return (
       <footer class={`${displayClass ?? ""}`}>
